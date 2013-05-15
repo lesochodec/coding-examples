043474e38454335848595fc795d0d738e7fc1c14
Jan Kmit <lesochodec@gmail.com>
2013-05-15 20:15:41 +0200

    Prve programy v bash-i a ruby.

---
+++ linux/prikazy.sh
@@ -0,0 +1,9 @@
+free -m	# stav ram pamete
+ls	# co je v aktualnom adresari?
+cd /	# id do korenoveho adresara / (/ = korenovy adresar)
+ls -l	# podrobny vypis adresara 
+cd	# id do domovsky adresara
+cd ~	# (~ skratka domovskeho adresara)
+# cd <adresar> - zmena aktualneho adresara na <adresar>
+pwd # zobrazenie kde som?
+# mkdir <adresar> - vytvor <adresar>
